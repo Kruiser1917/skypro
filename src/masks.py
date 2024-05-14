55be189ee091eaf82f1f39626e97943c4e7728f3
aptik <aptik@DESKTOP-SVN8V0A>
2024-05-14 11:59:42 +0300

    Добавлен .gitignore

--- src/masks.py
+++ src/masks.py
@@ -3,41 +3,37 @@
 
 def mask_card_number(card_number: str) -> str:
     """
-    Masks a credit card number, showing only the first 6 and the last 4 digits.
+    Маскирует номер кредитной карты, показывая только первые "
+    "6 и последние 4 цифры. "
 
-    Args:
-    card_number (str): The credit card number to be masked.
+    Аргументы:
+    card_number (str): Номер кредитной карты, который будет замаскирован.
 
-    Returns:
-    str: The masked credit card number in the format 'XXXX XX** **** XXXX'.
+    Возвращает:
+    str: Замаскированный номер кредитной карты "
+    "в формате 'XXXX XX** **** XXXX'.
     """
-    # номер карты состоит только из цифр и имеет правильную длину  (16 цифр)
-    if len(card_number) == 16 and card_number.isdigit():
-        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    # Проверяем, что номер карты состоит только из цифр и имеет допустимую длин
+    if 13 <= len(card_number) <= 19 and card_number.isdigit():
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     else:
-        raise ValueError(
-            "Invalid card number. Card number must have exactly 16 digits."
-        )
+        raise ValueError("Неверный номер карты. Номер карты должен содержать от 13 до 19 цифр.")
 
 
 def mask_account_number(account_number: str) -> str:
     """
-    Masks an account number, showing only the last 4 digits.
+    Маскирует номер счета, показывая только последние 4 цифры.
 
-    Args:
-    account_number (str): The account number to be masked.
+    Аргументы:
+    account_number (str): Номер счета, который будет замаскирован.
 
-    Returns:
-    str: The masked account number in the format '**XXXX'.
+    Возвращает:
+    str: Замаскированный номер счета в формате '**XXXX'.
     """
     # номер счета состоит только из цифр и имеет длину не менее 4 цифр
     if len(account_number) >= 4 and account_number.isdigit():
         return f"**{account_number[-4:]}"
     else:
         raise ValueError(
-            "Invalid account number. Account number must have at least 4 digits."
+            "Неверный номер счета. Номер счета должен содержать минимум 4 цифры."
         )
-
-
-print(mask_card_number("7000792289606361"))
-print(mask_account_number("73654108430135874305"))
--- src/widget.py
+++ src/widget.py
@@ -3,37 +3,33 @@ from src.masks import mask_account_number
 from src.masks import mask_card_number
 
 
-# Модуль widget с функцией для обработки входных данных и применения маскировки
-
 def universal_masking(input_data: str) -> str:
     """
     Маскирует номер кредитной карты или счета в зависимости от входных данных.
 
     Args:
-    input_data (str): Строка, содержащая тип и номер, например, "Visa 1234567890123456".
+        input_data (str): Входная строка с типом и номером (например, "Visa 1234567890123456").
 
     Returns:
-    str: Исходная строка с маскированным номером.
+        str: Исходная строка с маскированным номером.
     """
-    # Разбиваем входные данные, чтобы определить тип и номер
+
     parts = input_data.split()
-    account_type = parts[0]  # Тип, например "Visa", "MasterCard", "Счет" (для счетов)
-
-    # Определяем, входные данные относятся к карте или счету, основываясь на наличии "Счет"
-    if account_type == "Счет":
-        account_number = parts[1]
+    if parts[0] == "Счет":
+        account_number = parts[-1]
         masked_number = mask_account_number(account_number)
-        result = f"{account_type} {masked_number}"
+        return f"{' '.join(parts[:-1])} {masked_number}"
     else:
-        # Считаем, что остальные части - это номер карты, разделенный пробелами
-        card_number = "".join(parts[1:])
-        masked_number = mask_card_number(card_number)
-        result = f"{parts[0]} {masked_number[:4]} {masked_number[5:7]}{masked_number[7:9]} {masked_number[10:14]} {masked_number[15:]}"
+        card_number = ''.join(parts[-4:])
+        if len(card_number) == 16:
+            masked_number = mask_card_number(card_number)
+            return f"{' '.join(parts[:-4])} {masked_number[:4]} {masked_number[5:7]}{masked_number[7:9]} {masked_number[10:14]} {masked_number[15:]}"
+        else:
+            return f"{' '.join(parts[:-4])} Card number must be exactly 16 digits long"
 
-    return result
 
-
-# Модуль widget с функцией для преобразования даты и времени в дату
+# Функция для преобразования строки с датой и временем в формате
+# ISO в строку даты в формате DD.MM.YYYY
 
 def format_datetime_to_date(datetime_str: str) -> str:
     """
@@ -53,8 +49,18 @@ def format_datetime_to_date(datetime_str: str) -> str:
     formatted_date = f"{day}.{month}.{year}"
     return formatted_date
 
-# Примеры функций оставлены в комментариях для проверки формата
-# universal_masking("Visa Platinum 7000 7922 8960 6361")
-# universal_masking("Счет 73654108430135874305")
-# format_datetime_to_date("2018-07-11T02:26:18.671407")
 
+print(format_datetime_to_date("2018-07-11T02:26:18.671407"))
+
+# Примеры для тестирования
+test_inputs = [
+    "Visa Platinum 7000 7922 8960 6361",
+    "Maestro 1596 8378 6870 5199",
+    "Счет 73654108430135874305",
+    "MasterCard 7158 3007 3472 6758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831 9824 7673 7658"
+]
+
+for test_input in test_inputs:
+    print(universal_masking(test_input))
