e118a3b3fb6fdeb9a83bfd0b2bac012c772e1dcd
aptik <aptik@DESKTOP-SVN8V0A>
2024-05-16 14:13:43 +0300

    отредактировал код, убрал ошибки PEP8

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,54 @@
-# маскировки номеров карт и счетов
+def format_datetime_to_date(datetime_str: str) -> str:
+    """
+    Преобразует строку с датой и временем в формате ISO в строку даты в формате DD.MM.YYYY.
+
+    Args:
+    datetime_str (str): Строка даты и времени в формате ISO, например, "2018-07-11T02:26:18.671407".
+
+    Returns:
+    str: Строка даты в формате DD.MM.YYYY.
+    :param datetime_str:
+    :return:
+    """
+    date_part = datetime_str.split("T")[0]
+    year, month, day = date_part.split("-")
+    return f"{day}.{month}.{year}"
+
+
+def universal_masking(input_data: str) -> str:
+    """
+    Маскирует номер кредитной карты или счета в зависимости от входных данных.
+
+    Args:
+        input_data (str): Входная строка с типом и номером (например, "Visa 1234567890123456").
+
+    Returns:
+        str: Исходная строка с маскированным номером.
+    """
+    parts = input_data.split()
+    if parts[0] == "Счет":
+        account_number = parts[-1]
+        masked_number = mask_account_number(account_number)
+        return f"{' '.join(parts[:-1])} {masked_number}"
+    else:
+        card_number = ''.join(parts[-4:])
+        if len(card_number) == 16:
+            masked_number = mask_card_number(card_number)
+            return f"{' '.join(parts[:-4])} {masked_number[:4]} {masked_number[5:7]}{masked_number[7:9]} {masked_number[10:14]} {masked_number[15:]}"
+        else:
+            return f"{' '.join(parts[:-4])} Card number must be exactly 16 digits long"
 
 
 def mask_card_number(card_number: str) -> str:
     """
-    Маскирует номер кредитной карты, показывая только первые "
-    "6 и последние 4 цифры. "
+    Маскирует номер кредитной карты, показывая только первые 6 и последние 4 цифры.
 
-    Аргументы:
+    Args:
     card_number (str): Номер кредитной карты, который будет замаскирован.
 
-    Возвращает:
-    str: Замаскированный номер кредитной карты "
-    "в формате 'XXXX XX** **** XXXX'.
+    Returns:
+    str: Замаскированный номер кредитной карты в формате 'XXXX XX** **** XXXX'.
     """
-    # Проверяем, что номер карты состоит только из цифр и имеет допустимую длин
     if 13 <= len(card_number) <= 19 and card_number.isdigit():
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     else:
@@ -24,13 +59,12 @@ def mask_account_number(account_number: str) -> str:
     """
     Маскирует номер счета, показывая только последние 4 цифры.
 
-    Аргументы:
+    Args:
     account_number (str): Номер счета, который будет замаскирован.
 
-    Возвращает:
+    Returns:
     str: Замаскированный номер счета в формате '**XXXX'.
     """
-    # номер счета состоит только из цифр и имеет длину не менее 4 цифр
     if len(account_number) >= 4 and account_number.isdigit():
         return f"**{account_number[-4:]}"
     else:
