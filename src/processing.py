ba6e4ecf451aae6269051a571e61f5d0ed737a83
aptik <aptik@DESKTOP-SVN8V0A>
2024-05-16 14:06:28 +0300

    обавлены функции для фильтрации и сортировки списка словарей

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+
 from typing import List, Dict
 
 def filter_by_state(records: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
@@ -13,7 +14,6 @@ def filter_by_state(records: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     """
     return [record for record in records if record.get('state') == state]
 
-from typing import List, Dict
 
 def sort_by_date(records: List[Dict], descending: bool = True) -> List[Dict]:
     """
@@ -27,28 +27,3 @@ def sort_by_date(records: List[Dict], descending: bool = True) -> List[Dict]:
     List[Dict]: Новый список, отсортированный по ключу 'date'.
     """
     return sorted(records, key=lambda x: x['date'], reverse=descending)
-
-
-records = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
-# Фильтрация по состоянию EXECUTED
-filtered_records = filter_by_state(records)
-print(filtered_records)
-
-# Фильтрация по состоянию CANCELED
-filtered_records_canceled = filter_by_state(records, 'CANCELED')
-print(filtered_records_canceled)
-
-# Сортировка по дате по убыванию
-sorted_records_desc = sort_by_date(records)
-print(sorted_records_desc)
-
-# Сортировка по дате по возрастанию
-sorted_records_asc = sort_by_date(records, descending=False)
-print(sorted_records_asc)
-
